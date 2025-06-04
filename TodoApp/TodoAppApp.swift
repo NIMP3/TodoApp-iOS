23f34937a9abbfb327940274a7c06cf3e6e85a20
Edwin Yovany <yovany.dev@gmail.com>
2025-06-03 19:14:30 -0500

    [BUILD] (Initial): Initial project files deployed

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Edwin Yovany on 3/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
